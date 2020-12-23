2f44419322763d905370a134876fd971d3337475
vsaini1 <74753490+vsaini1@users.noreply.github.com>
2020-12-23 15:23:15 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,4 +2,5 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" { default = "us-east-1" }
 variable "website_bucket_name" { default = "mywebsite.com" }
-# URL will be: http://<website_bucket-name>-web.s3.us-east-1.amazonaws.com/index.html
+#Please replace mywebsite.com to desired name in above variable so that URL will be:
+#http://<website_bucket-name>.s3-website-us-east-1.amazonaws.com/index.html
